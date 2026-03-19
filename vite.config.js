e03c2651c4cad1c6eb521dd5c69f404fa248f1fd
sryanc1 <craigpryan80@gmail.com>
2026-03-19 15:30:42 +1030

    Fix base path capitalisation to match repo name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/project-psalmist/',
+  base: '/Project-Psalmist/',
   root: 'src',
   build: {
     outDir: '../dist',
